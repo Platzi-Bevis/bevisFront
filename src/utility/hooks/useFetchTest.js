04bd297c9d0491bda7dae89e5beeef1533b747b3
Alejandro Medina <5almero5@gmail.com>
2020-12-18 14:37:32 -0600

    Add axios dependencie fix typo

--- src/utility/hooks/useFetchTest.js
+++ src/utility/hooks/useFetchTest.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react'
 import { GetTest } from '../api'
 
-export function useCategoriesData (id_course, id_material) {
+export function useTestData(idCourse, idMaterial) {
   const [test, setTest] = useState(null)
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setLoading(true)
-    GetTest(id_course, id_material)
-      .then(res => res.json())
-      .then(response => setTest(response))
+    GetTest(idCourse, idMaterial)
+      .then((res) => res.json())
+      .then((response) => setTest(response))
       .finally(() => setLoading(false))
   }, [])
   return { test, loading }
