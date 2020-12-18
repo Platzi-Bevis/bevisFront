12cd60b64673c608003eede431714f08db92c6d4
Alejandro Medina <5almero5@gmail.com>
2020-12-18 00:27:13 -0600

    Add [CD] github action
    The CD use wolftrax personal developer tocken, should deploy at gh-pages

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+console.log('%c Secret API URL', 'color: green;',process.env.APP_API_URL)
 ReactDOM.render(<App />, document.getElementById('app'))
