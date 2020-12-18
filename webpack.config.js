66592703ce5ba88980593880eb77fbd0405a0351
Juan Camilo Rico <juancarico92@gmail.com>
2020-12-17 23:56:41 -0500

    fix(webpack): webpack server command in package.json and typo in webpack.config.js fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: '.src/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
