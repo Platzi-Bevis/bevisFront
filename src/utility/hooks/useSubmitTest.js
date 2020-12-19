63283def6adfc0b758861964cc2be96ea4f79dca
Alejandro Medina <5almero5@gmail.com>
2020-12-18 19:57:19 -0600

    Add post code

---
+++ src/utility/hooks/useSubmitTest.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { PostTest } from '../api'
+
+export function useSubmitTest(idCourse, idMaterial, { language, code }) {
+  const [solution, setSolution] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  setLoading(true)
+
+  PostTest(idCourse, idMaterial, { language, code })
+    .then((res) => res.json())
+    .then((response) => setSolution(response))
+    .finally(() => setLoading(false))
+
+  return { solution, loading }
+}
